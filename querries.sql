706edab82d2597134330767dcc055d23d3fbf4a7
arnautlu <arnautlu@hs-pforzheim.de>
2025-01-08 19:52:50 +0100

    update

--- querries.sql
+++ querries.sql
@@ -1,26 +1,27 @@
 --Abfragen für das Projekt
 
 --1. Welches Geschlecht ist der gefährlichere Fahrer?
-SELECT "Sex_of_driver", "Sex_of_casualty", COUNT(*) AS count
+SELECT "Sex_of_driver" AS "Sex of driver", "Sex_of_casualty" AS "Sex of casualty", COUNT(*) AS "Amount of accidents"
 FROM "RoadAccidentsSeverity"
 WHERE "Sex_of_driver" NOT IN ('Unknown', 'na')
 AND "Sex_of_casualty" NOT IN ('Unknown', 'na')
-GROUP BY "Sex_of_driver", "Sex_of_casualty";
+GROUP BY "Sex_of_driver", "Sex_of_casualty"
+ORDER BY "Amount of accidents" DESC;
 
 
 --2. Welche Altersgruppe ist am häufigsten in Unfälle verwickelt? Spielt das Bildungslevel dabei ein Rolle?
-SELECT "Educational_level", "Age_band_of_driver", COUNT(*) AS count
+SELECT "Educational_level" AS "Educational level", "Age_band_of_driver" AS "Age band of driver", COUNT(*) AS "Amount of accidents"
 FROM "RoadAccidentsSeverity"
 WHERE "Educational_level" NOT IN ('') AND "Age_band_of_driver" NOT IN ('Unknown', '')
 GROUP BY "Educational_level", "Age_band_of_driver"
-ORDER BY count DESC;
+ORDER BY "Amount of accidents" DESC;
 
 
 --3. Welche Länder haben die meisten Verkehrunfälle die durch Alkohol verursacht wurden?
 SELECT 
     g."Country",
-    SUM(g."Injuries") AS TotalInjuries,
-    SUM(g."Fatalities") AS TotalFatalities
+    SUM(g."Injuries") AS "Total injuries",
+    SUM(g."Fatalities") AS "Total fatalities"
 FROM 
     "GlobalTrafficAccidentsandRoadSafety" g
 JOIN 
@@ -28,13 +29,13 @@ JOIN
 GROUP BY 
     g."Country"
 ORDER BY 
-    TotalFatalities DESC, TotalInjuries DESC;
+    "Total fatalities" DESC, "Total injuries" DESC;
 
 
 --4. Spielen Lichtverhätnisse eine Rolle bei Roadaccidents?
 SELECT 
-    "Light_conditions", 
-    COUNT(*) AS accident_count
+    "Light_conditions" AS "Light conditions", 
+    COUNT(*) AS "Amount of accidents"
 FROM 
     "RoadAccidentsSeverity"
 WHERE 
@@ -42,28 +43,28 @@ WHERE
 GROUP BY 
     "Light_conditions"
 ORDER BY 
-    accident_count DESC;
+    "Amount of accidents" DESC;
 
 
 --5. Welche Fahrzeuge die ... fahren sollte man vermeiden?
 SELECT 
-    "Vehicle_movement", 
-    COUNT(*) AS accident_count
+    "Vehicle_movement" AS "Vehicle movement", 
+    COUNT(*) AS "Amount of accidents"
 FROM 
     "RoadAccidentsSeverity"
 WHERE 
-    "Vehicle_movement" NOT IN ('Unknown', 'na')
+    "Vehicle_movement" NOT IN ('Unknown', 'na', '')
 GROUP BY 
     "Vehicle_movement"
 ORDER BY 
-    accident_count DESC;
+    "Amount of accidents" DESC;
 
 
 --6 Wie Representativ sind sind 100.00 Menschen bzgl. der gesamten Population?                  keine Werte!!!
 SELECT 
     e."Location",
-   ( AVG(e."Value")/100000) * c."population" AS AvgEstimatedDeathRatehochgerechnet,
-    SUM(r."FactValueNumeric") AS TotalActualDeaths
+   (AVG(e."Value")/100000) * c."population" AS "100.000-value projected to the actual population",
+    SUM(r."FactValueNumeric") AS  "Amount of deaths"
 FROM 
     "Estimatedroadtrafficdeathrate(per100000population)" e
 JOIN 
@@ -73,20 +74,20 @@ JOIN
 GROUP BY 
     e."Location", c."population"
 ORDER BY 
-    AvgEstimatedDeathRatehochgerechnet DESC;
+    "100.000-value projected to the actual population" DESC;
 
 
 -- 7. In welchen Ländern gibt es die meisten Verkehrstoten?                                 keine Aussagekraft!!!
-SELECT "Location", SUM("Value") AS total_vehicle_deaths
+SELECT "Location", SUM("Value") AS "Amount of deaths"
 FROM "deathsvehicle"
 GROUP BY "Location"
-ORDER BY total_vehicle_deaths DESC;
+ORDER BY "Amount of deaths" DESC;
 
 
 --8. Wo werden am haufugsten Fahrradfahrer verletzt?
 SELECT 
     "Country",
-    SUM("Injuries") AS TotalInjuries
+    SUM("Injuries") AS "Total injuries"
 FROM 
     "GlobalTrafficAccidentsandRoadSafety"
 WHERE 
@@ -94,13 +95,13 @@ WHERE
 GROUP BY 
     "Country", "Accident Type"
 ORDER BY 
-    TotalInjuries DESC;
+    "Total injuries" DESC;
 
 
 --9. Stehen diese (siehe 8.) in einem Zusammenhang mit Fahrradtoten? Ist es möglcih eine Ration zu schätzen
 SELECT 
     "Country",
-    SUM("Fatalities") AS TotalFatalities
+    SUM("Fatalities") AS "Total fatalities"
 FROM 
     "GlobalTrafficAccidentsandRoadSafety"
 WHERE 
@@ -108,19 +109,19 @@ WHERE
 GROUP BY 
     "Country", "Accident Type"
 ORDER BY 
-    TotalFatalities DESC;
+    "Total fatalities" DESC;
 
 
---10. Welche Masßnahmen mussten am meisten eingesetzt werden?                               in welchem Zusammenhang?
+--10. Welche Masßnahmen mussten am meisten eingesetzt werden um Verkehrsunfälle zu vermeiden?                               in welchem Zusammenhang?
 SELECT 
     "Road Safety Measures",
-    COUNT(*) AS UsageCount
+    COUNT(*) AS "Amount of declarations"
 FROM 
     "GlobalTrafficAccidentsandRoadSafety"
 GROUP BY 
     "Road Safety Measures"
 ORDER BY 
-    UsageCount DESC;
+    "Amount of declarations" DESC;
 
 
 --11. Sind Regierungsmitglieder bessere Fahrer?                                         keine Aussagekraft!!!
@@ -128,14 +129,14 @@ SELECT
     CASE 
         WHEN "Owner_of_vehicle" = 'Governmental' THEN 'Governmental'
         ELSE 'Other'
-    END AS OwnershipType,
-    COUNT(*) AS AccidentCount
+    END AS "Ownership",
+    COUNT(*) AS "Amount of accidents"
 FROM 
     "RoadAccidentsSeverity"
 GROUP BY 
-    OwnershipType
+    "Ownership"
 ORDER BY 
-    AccidentCount DESC;
+    "Amount of accidents" DESC;
 
 
 --12. In welchen Ländern gibt es die meisten Fahrzeugtote?
@@ -154,12 +155,24 @@ FROM (
     LIMIT 5
 ) AS subquery;
 
+    --mit der Anzahl an Toten
+    SELECT 
+        "Location",
+        COUNT(*) AS "Amount of deaths"
+    FROM 
+        "deathsvehicle"
+    GROUP BY 
+        "Location"
+    ORDER BY 
+        "Amount of deaths" DESC
+    LIMIT 5;
+
 
 --13. Welcher Wochentag ist für welche Altersgruppe am gefährlichsten?
 WITH RankedEntries AS (
     SELECT 
-        "Age_band_of_driver",
-        "Day_of_week",
+        "Age_band_of_driver" AS "Age band of driver",
+        "Day_of_week" AS "Day of week",
         COUNT(*) AS EntryCount,
         ROW_NUMBER() OVER (PARTITION BY "Age_band_of_driver" ORDER BY COUNT(*) DESC) AS rn
     FROM 
@@ -170,20 +183,20 @@ WITH RankedEntries AS (
         "Age_band_of_driver", "Day_of_week"
 )
 SELECT 
-    "Age_band_of_driver",
-    "Day_of_week"
+    "Age band of driver",
+    "Day of week"
 FROM 
     RankedEntries
 WHERE 
     rn = 1
 ORDER BY 
-    "Age_band_of_driver";
+    "Age band of driver";
     
 
 --14. In welchen Gebieten einer Stadt werden die meisten Unfälle verursacht?
 SELECT 
-    "Area_accident_occured",
-    COUNT(*) AS AccidentCount
+    "Area_accident_occured" AS "Accident area",
+    COUNT(*) AS "Amount of accidents"
 FROM 
     "RoadAccidentsSeverity"
 WHERE 
@@ -191,14 +204,14 @@ WHERE
 GROUP BY 
     "Area_accident_occured"
 ORDER BY 
-    AccidentCount DESC
+    "Amount of accidents" DESC
 LIMIT 3;
 
 
 --15. Unter welcher Regierng(-sform) sterben mehr Leute durch Verkehrunfälle?                   keine daten!!!
 SELECT 
-    p."government",
-    SUM(r."FactValueNumeric") AS TotalRoadTrafficDeaths
+    p."government" AS "Type of government",
+    SUM(r."FactValueNumeric") AS "Amount of deaths"
 FROM 
     "politics" p
 JOIN 
@@ -208,7 +221,7 @@ JOIN
 GROUP BY 
     p."government"
 ORDER BY 
-    TotalRoadTrafficDeaths DESC
+    "Amount of deaths" DESC
 LIMIT 10;
 
 
